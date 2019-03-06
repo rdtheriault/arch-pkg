5a4df19487606bb5d86620786a69a507dc824c28
Dovah Merith <merith.tk@gmail.com>
2019-03-06 10:49:29 -0800

    first release

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
-if [ ! $(whoami)="root"]; then 
-    echo "Need root perms to install, use sudo or run as root"
-else 
-    #cp $PWD/bin/yay /usr/bin/yay
-    #cp $PWD/bin/pkg /usr/bin/pkg
-    echo "sudo check works"
-fi
-
+if [ ! $(whoami) = "root" ]; then 
+    echo please run as root
+    exit
+else
+    if [ ! $(which yay) = "" ]; then 
+        echo "preinstalled yay found, installing to /usr/bin/wrap"
+        cp bin/wrap /usr/bin/wrap
+        chmod +x /usr/bin/wrap
+    else   
+        echo "yay not found, installing to /usr/bin/yay"
+        echo "installing wrap to /usr/bin/wrap"
+        cp bin/yay /usr/bin/yay
+        cp bin/wrap /usr/bin/wrap
+        chmod +x /usr/bin/wrap
+        chmod +x /usr/bin/yay
+    fi
+fi[]
 exit
